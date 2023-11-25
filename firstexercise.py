1d6b4392696bd6e15ff350ae3de52a52c66fb06b
Mohammad Habibur Rahman Molla Molla <133572669+saky7009@users.noreply.github.com>
2023-11-25 23:57:38 +0300

    Add files via upload

---
+++ firstexercise.py
@@ -0,0 +1,17 @@
+print("test")
+print("hello world")
+print("love")
+A = 12
+B = 13
+print(A+B)
+print(A-B)
+print(A*B)
+print(A%B)
+print("hello+Banglade")
+print({"helo", " KAU community"})
+print("Bangla is the most uses language in the worls" "\n"" it is after line")
+print("Bangla is the most uses language in the worls" "\n" "\n" "\n" "it is the after line")
+print('''is is the first line "
+      "it is the second line"
+      "It is the third line''')
+print()
